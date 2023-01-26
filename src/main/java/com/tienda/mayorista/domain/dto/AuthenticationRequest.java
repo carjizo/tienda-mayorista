ac1c8dbd936dfae2840ae6d54f93c6eb71597412
carjizo <carloshernanjimenez876@gmail.com>
2023-01-26 09:00:05 -0500

    autorizacion con jwt

--- src/main/java/com/tienda/mayorista/domain/dto/AuthenticationRequest.java
+++ src/main/java/com/tienda/mayorista/domain/dto/AuthenticationRequest.java
@@ -1,4 +1,22 @@
 package com.tienda.mayorista.domain.dto;
 
 public class AuthenticationRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
